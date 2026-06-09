8d0c168d1ce91b826be80e5aa4de12b7337aaf56
段志剑 <3433160091@qq.com>
2026-06-09 13:11:57 +0800

    feat(import): add OCR toggle to import request

--- lib/services/import_pipeline/import_parse_request.dart
+++ lib/services/import_pipeline/import_parse_request.dart
@@ -2,6 +2,7 @@ class ImportParseRequest {
   final List<String> filePaths;
   final List<String> fileNames;
   final bool useVisionEngine;
+  final bool useOcrEngine;
   final int maxConcurrency;
   final String taskId;
 
@@ -9,6 +10,7 @@ class ImportParseRequest {
     required this.filePaths,
     required this.fileNames,
     required this.useVisionEngine,
+    this.useOcrEngine = false,
     required this.maxConcurrency,
     required this.taskId,
   });
